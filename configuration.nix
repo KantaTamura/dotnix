9608baafd97a34ed2a0cca1d08f186791b2b3a99
KantaTamura <tkanta496@gmail.com>
2026-03-04 21:22:21 +0900

    nixos: configure static IP networking
    Configure a static IPv4 address for interface enp3s0.
    
    - IP: 192.168.10.3/24
    - gateway: 192.168.10.1
    - DNS: 1.1.1.1, 8.8.8.8
    
    Signed-off-by: KantaTamura <tkanta496@gmail.com>

--- configuration.nix
+++ configuration.nix
@@ -31,6 +31,20 @@
   # Configure network connections interactively with nmcli or nmtui.
   networking.networkmanager.enable = true;
 
+  networking.interfaces.enp3s0.ipv4.addresses = [
+    {
+      address = "192.168.10.3";
+      prefixLength = 24;
+    }
+  ];
+
+  networking.defaultGateway = "192.168.10.1";
+
+  networking.nameservers = [
+    "1.1.1.1"
+    "8.8.8.8"
+  ];
+
   # Set your time zone.
   # time.timeZone = "Europe/Amsterdam";
   time.timeZone = "Asia/Tokyo";
