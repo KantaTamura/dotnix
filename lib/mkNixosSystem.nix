bc5ac9d307d857cd7c766ff2e25db925df590ca6
KantaTamura <tkanta496@gmail.com>
2026-05-26 00:12:11 +0900

    style: Format Nix expressions
    Signed-off-by: KantaTamura <tkanta496@gmail.com>

--- lib/mkNixosSystem.nix
+++ lib/mkNixosSystem.nix
@@ -28,33 +28,32 @@ nixpkgs.lib.nixosSystem {
       ;
   };
 
-  modules =
-    [
-      (self + /profiles/nixos/base.nix)
-      (self + /hosts/nixos/${hostName})
-    ]
-    ++ lib.optionals (userName != null) [
-      (self + /modules/nixos/users/${userName}.nix)
-      home-manager.nixosModules.home-manager
-      {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.extraSpecialArgs = {
-          inherit
-            inputs
-            self
-            userName
-            homeDirectory
-            ;
-          username = userName;
-        };
-        home-manager.users."${userName}" = {
-          imports = [
-            (self + /home/users/${userName})
-          ]
-          ++ extraHomeModules;
-        };
-      }
-    ]
-    ++ extraModules;
+  modules = [
+    (self + /profiles/nixos/base.nix)
+    (self + /hosts/nixos/${hostName})
+  ]
+  ++ lib.optionals (userName != null) [
+    (self + /modules/nixos/users/${userName}.nix)
+    home-manager.nixosModules.home-manager
+    {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.extraSpecialArgs = {
+        inherit
+          inputs
+          self
+          userName
+          homeDirectory
+          ;
+        username = userName;
+      };
+      home-manager.users."${userName}" = {
+        imports = [
+          (self + /home/users/${userName})
+        ]
+        ++ extraHomeModules;
+      };
+    }
+  ]
+  ++ extraModules;
 }
--- modules/nixos/users/kanta.nix
+++ modules/nixos/users/kanta.nix
@@ -4,7 +4,10 @@
     isNormalUser = true;
     description = "Kanta Tamura";
     shell = pkgs.zsh;
-    extraGroups = [ "wheel" "docker" ];
+    extraGroups = [
+      "wheel"
+      "docker"
+    ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIUXZIigZ+berBnrBxykvRe9Sn6D+URl7gy+cRDVxJqv"
     ];
