295f81d8545fd7b4f041b87d1cdda211d6677f63
KantaTamura <tkanta496@gmail.com>
2026-05-26 00:24:23 +0900

    style: Format mkHomeTestVm
    Signed-off-by: KantaTamura <tkanta496@gmail.com>

--- lib/mkHomeTestVm.nix
+++ lib/mkHomeTestVm.nix
@@ -25,60 +25,64 @@ nixpkgs.lib.nixosSystem {
   };
 
   modules = [
-    ({ modulesPath, pkgs, ... }: {
-      imports = [
-        (modulesPath + "/virtualisation/qemu-vm.nix")
-        home-manager.nixosModules.home-manager
-        (self + /modules/common/core.nix)
-        (self + /modules/common/nix.nix)
-        (self + /modules/common/shell/zsh.nix)
-        (self + /modules/nixos/i18n/locale-ja.nix)
-      ];
-
-      networking.hostName = hostName;
-      system.stateVersion = "25.11";
-
-      users.users.${userName} = {
-        isNormalUser = true;
-        description = userName;
-        extraGroups = [ "wheel" ];
-        initialPassword = "nixos";
-        shell = pkgs.zsh;
-      };
-
-      services.getty.autologinUser = userName;
-
-      home-manager.useGlobalPkgs = true;
-      home-manager.useUserPackages = true;
-      home-manager.extraSpecialArgs = {
-        inherit
-          inputs
-          self
-          homeDirectory
-          ;
-        username = userName;
-      };
-      home-manager.users.${userName} = {
+    (
+      { modulesPath, pkgs, ... }:
+      {
         imports = [
-          (self + /home/users/${userName})
-        ] ++ extraHomeModules;
-      };
-
-      virtualisation = {
-        memorySize = 4096;
-        cores = 2;
-        graphics = false;
-        forwardPorts = [
-          {
-            from = "host";
-            host.port = 2222;
-            guest.port = 22;
-          }
+          (modulesPath + "/virtualisation/qemu-vm.nix")
+          home-manager.nixosModules.home-manager
+          (self + /modules/common/core.nix)
+          (self + /modules/common/nix.nix)
+          (self + /modules/common/shell/zsh.nix)
+          (self + /modules/nixos/i18n/locale-ja.nix)
         ];
-      };
 
-      documentation.enable = false;
-      services.openssh.enable = true;
-    })
+        networking.hostName = hostName;
+        system.stateVersion = "25.11";
+
+        users.users.${userName} = {
+          isNormalUser = true;
+          description = userName;
+          extraGroups = [ "wheel" ];
+          initialPassword = "nixos";
+          shell = pkgs.zsh;
+        };
+
+        services.getty.autologinUser = userName;
+
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.extraSpecialArgs = {
+          inherit
+            inputs
+            self
+            homeDirectory
+            ;
+          username = userName;
+        };
+        home-manager.users.${userName} = {
+          imports = [
+            (self + /home/users/${userName})
+          ]
+          ++ extraHomeModules;
+        };
+
+        virtualisation = {
+          memorySize = 4096;
+          cores = 2;
+          graphics = false;
+          forwardPorts = [
+            {
+              from = "host";
+              host.port = 2222;
+              guest.port = 22;
+            }
+          ];
+        };
+
+        documentation.enable = false;
+        services.openssh.enable = true;
+      }
+    )
   ];
 }
