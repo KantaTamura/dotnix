f22bcf6673dbefeae5721f0b7da5909d6e9da50b
KantaTamura <tkanta496@gmail.com>
2026-05-28 12:46:57 +0900

    nvim: migrate treesitter config to main branch
    Switch nvim-treesitter to the main branch and replace the old
    configs.setup()-based configuration with the new explicit
    vim.treesitter.start() setup.
    
    Install parsers with require("nvim-treesitter").install() and enable
    Tree-sitter per filetype using a FileType autocmd. Also configure the
    new Tree-sitter indentexpr and disable nvim-treesitter-refactor for now
    because it depends on the old nvim-treesitter setup.
    
    Signed-off-by: KantaTamura <tkanta496@gmail.com>

--- config/nvim/lua/plugins/syntax-highlight.lua
+++ config/nvim/lua/plugins/syntax-highlight.lua
@@ -1,77 +1,108 @@
+local parsers = {
+	"go",
+	"gosum",
+	"gomod",
+	"gowork",
+	"lua",
+	"python",
+	"rust",
+	"typescript",
+	"tsx",
+	"vimdoc",
+	"vim",
+	"kotlin",
+	"dockerfile",
+	"json",
+	"json5",
+	"terraform",
+	"hcl",
+	"bash",
+	"c",
+	"html",
+	"javascript",
+	"jsdoc",
+	"luadoc",
+	"luap",
+	"markdown",
+	"markdown_inline",
+	"query",
+	"regex",
+	"yaml",
+	"zig",
+	"bibtex",
+	"latex",
+}
+
+local treesitter_filetypes = {
+	"bash",
+	"bib",
+	"c",
+	"dockerfile",
+	"go",
+	"gomod",
+	"gosum",
+	"gowork",
+	"hcl",
+	"help",
+	"html",
+	"javascript",
+	"javascriptreact",
+	"json",
+	"json5",
+	"jsonc",
+	"kotlin",
+	"lua",
+	"luadoc",
+	"markdown",
+	"python",
+	"query",
+	"regex",
+	"rust",
+	"sh",
+	"terraform",
+	"tex",
+	"typescript",
+	"typescriptreact",
+	"vim",
+	"yaml",
+	"zig",
+}
+
 return {
+	{
+		"nvim-treesitter/nvim-treesitter-refactor",
+		enabled = false,
+	},
 	{
 		-- Highlight, edit, and navigate code
 		"nvim-treesitter/nvim-treesitter",
-		event = { "BufReadPost", "BufNewFile" },
+		branch = "main",
+		lazy = false,
 		build = ":TSUpdate",
-		cmd = { "TSUpdate", "TSInstall", "TSUninstall" },
 		cond = true,
-		opts = {
-			ensure_installed = {
-				"go",
-				"gosum",
-				"gomod",
-				"gowork",
-				"lua",
-				"python",
-				"rust",
-				"typescript",
-				"tsx",
-				"vimdoc",
-				"vim",
-				"kotlin",
-				"dockerfile",
-				"json",
-				"json5",
-				"jsonc",
-				"terraform",
-				"hcl",
-				"bash",
-				"c",
-				"html",
-				"javascript",
-				"jsdoc",
-				"luadoc",
-				"luap",
-				"markdown",
-				"markdown_inline",
-				"query",
-				"regex",
-				"yaml",
-				"zig",
-				"bibtex",
-				"latex",
-			},
-			-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<c-space>",
-					node_incremental = "<c-space>",
-					scope_incremental = "<c-s>",
-					node_decremental = "<M-space>",
-				},
-			},
-		},
-		config = function(_, opts)
-			-- require("nvim-treesitter.configs").setup(opts)
+		config = function()
+			vim.treesitter.language.register("json", "jsonc")
+			require("nvim-treesitter").install(parsers)
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = treesitter_filetypes,
+				callback = function()
+					local ok = pcall(vim.treesitter.start)
+					if ok then
+						vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					end
+				end,
+			})
+
 			-- Diagnostic keymaps
-			vim.keymap.set("n", "[d", function ()
+			vim.keymap.set("n", "[d", function()
 				vim.diagnostic.jump({ count = -vim.v.count1, float = true })
 			end, { desc = "Go to previous diagnostic message", silent = true })
-			vim.keymap.set("n", "]d", function ()
-				vim.diagnostic.jump({ count =  vim.v.count1, float = true })
+			vim.keymap.set("n", "]d", function()
+				vim.diagnostic.jump({ count = vim.v.count1, float = true })
 			end, { desc = "Go to next diagnostic message", silent = true })
 			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 			vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
-		end
+		end,
 	},
-	{
-		"nvim-treesitter/nvim-treesitter-refactor",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
-		event = { "BufReadPost", "BufNewFile" },
-	}
 }
